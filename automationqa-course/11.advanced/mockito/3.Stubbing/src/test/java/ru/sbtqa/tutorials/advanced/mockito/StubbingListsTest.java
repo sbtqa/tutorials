543a04fa8e0833bea1fd50a0d490975efe23d1d8
Vladimir Zhilin <VIZhilin.SBT@sberbank.ru>
2018-04-30 18:27:17 +0300

    Добавил, в stubbing, что нельзя final'ы mock'ать.

--- automationqa-course/11.advanced/mockito/3.Stubbing/src/test/java/ru/sbtqa/tutorials/advanced/mockito/StubbingListsTest.java
+++ automationqa-course/11.advanced/mockito/3.Stubbing/src/test/java/ru/sbtqa/tutorials/advanced/mockito/StubbingListsTest.java
@@ -1,13 +1,16 @@
 package ru.sbtqa.tutorials.advanced.mockito;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.util.List;
 
+import static java.lang.Integer.valueOf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Samples for stubbing.
@@ -15,26 +18,27 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class StubbingListsTest {
     // В данном контексте SuppressWarnings допустим, но только для unchecked - мы создаём mockedList, который будет хранить только Integer'ы
     @SuppressWarnings("unchecked")
-    private final List<Integer> mockedList = Mockito.mock(List.class);
+    private final List<Integer> mockedList = mock(List.class);
     // Можно мокать не только интерфейсы, как в этом примере, но и конкретные реализации
 
     @Test
     void testStubbingMethodWithReturnValue() {
         // Создаётся заглушка - если у объекта mockedList будет вызван метод get с аргументом 0, то необходимо вернуть 1
-        Mockito.when(mockedList.get(0)).thenReturn(1);
+        when(mockedList.get(0)).thenReturn(1);
         // Создаётся заглушка - если у объекта mockedList будет вызван метод get с аргументом 1, то необходимо вернуть 2
-        Mockito.when(mockedList.get(1)).thenReturn(2);
+        when(mockedList.get(1)).thenReturn(2);
         // Создаётся заглушка - если у объекта mockedList будет вызван метод get с аргументом 2, то бросить исключение RuntimeException
-        Mockito.when(mockedList.get(2)).thenThrow(new RuntimeException());
+        when(mockedList.get(2)).thenThrow(new RuntimeException());
         // Создаётся заглушка - если у объекта mockedList будет вызван метод size, то необходимо вернуть 1000
-        Mockito.when(mockedList.size()).thenReturn(1000);
+        when(mockedList.size()).thenReturn(1000);
 
         // Assert'ы здесь с точки зрения простого отображения ожидаемого и фактического результата, так как проверять mockito не является нашей целью
+        // Не путайте в assert'ах местами значения - обратите внимание, слева expected, справа actual.
 
         // Вернётся 1, так как была создана заглушка для вызова метода get cо значением аргумента 0
-        assertEquals(Integer.valueOf(1), mockedList.get(0));
+        assertEquals(valueOf(1), mockedList.get(0));
         // Вернётся 2, так как была создана заглушка для вызова метода get cо значением аргумента 1
-        assertEquals(Integer.valueOf(2), mockedList.get(1));
+        assertEquals(valueOf(2), mockedList.get(1));
         // Будет брошено исключение RuntimeException - была создана соответствующая заглушка
         assertThrows(RuntimeException.class, () -> mockedList.get(2));
         // Вернётся 1000 - для вызова метода size была создана соответствующая заглушка
@@ -53,48 +57,48 @@ class StubbingListsTest {
     @Test
     void testStubbingMethodsWithoutReturnValue() {
         // Если у метода нет возвращаемого значения (void), то придётся в начале написать doThrow, чтобы реализовать DSL конструкцию
-        Mockito.doThrow(new RuntimeException()).when(mockedList).clear();
+        doThrow(new RuntimeException()).when(mockedList).clear();
 
         assertThrows(RuntimeException.class, mockedList::clear);
     }
 
     @Test
     void testMultipleInvocationsExample1() {
-        Mockito.when(mockedList.get(0)).thenReturn(1);
+        when(mockedList.get(0)).thenReturn(1);
 
         // Если один раз заглушку написали, то она будет постоянно возвращаться
 
-        assertEquals(Integer.valueOf(1), mockedList.get(0));
-        assertEquals(Integer.valueOf(1), mockedList.get(0));
-        assertEquals(Integer.valueOf(1), mockedList.get(0));
-        assertEquals(Integer.valueOf(1), mockedList.get(0));
-        assertEquals(Integer.valueOf(1), mockedList.get(0));
+        assertEquals(valueOf(1), mockedList.get(0));
+        assertEquals(valueOf(1), mockedList.get(0));
+        assertEquals(valueOf(1), mockedList.get(0));
+        assertEquals(valueOf(1), mockedList.get(0));
+        assertEquals(valueOf(1), mockedList.get(0));
     }
 
     @Test
     void testMultipleInvocationsExample2() {
-//        Mockito.when(mockedList.get(0)).thenReturn(1).thenReturn(10).thenReturn(100);
-        Mockito.when(mockedList.get(0)).thenReturn(1, 10, 100); // Сокращённая запись того же
+//        when(mockedList.get(0)).thenReturn(1).thenReturn(10).thenReturn(100);
+        when(mockedList.get(0)).thenReturn(1, 10, 100); // Сокращённая запись того же
 
         // Если несколько раз задали возвращаемое значение, то пойдут по очереди.
         // Если возвращаемых значений не останется, то будет возвращаться последнее.
 
-        assertEquals(Integer.valueOf(1), mockedList.get(0));
-        assertEquals(Integer.valueOf(10), mockedList.get(0));
-        assertEquals(Integer.valueOf(100), mockedList.get(0));
-        assertEquals(Integer.valueOf(100), mockedList.get(0));
-        assertEquals(Integer.valueOf(100), mockedList.get(0));
+        assertEquals(valueOf(1), mockedList.get(0));
+        assertEquals(valueOf(10), mockedList.get(0));
+        assertEquals(valueOf(100), mockedList.get(0));
+        assertEquals(valueOf(100), mockedList.get(0));
+        assertEquals(valueOf(100), mockedList.get(0));
     }
 
     @Test
     void testMultipleInvocationsExample3() {
         // + thenThrow не обязательно нужен объект, достаточно указать какого типа должно быть исключение
-        Mockito.when(mockedList.get(0)).thenReturn(1).thenThrow(RuntimeException.class);
+        when(mockedList.get(0)).thenReturn(1).thenThrow(RuntimeException.class);
 
         // Если несколько раз задали возвращаемое значение, то пойдут по очереди.
         // Если возвращаемых значений не останется, то будет возвращаться последнее.
 
-        assertEquals(Integer.valueOf(1), mockedList.get(0));
+        assertEquals(valueOf(1), mockedList.get(0));
         assertThrows(RuntimeException.class, () -> mockedList.get(0));
         assertThrows(RuntimeException.class, () -> mockedList.get(0));
         assertThrows(RuntimeException.class, () -> mockedList.get(0));
