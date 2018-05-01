5685b109f544886d8e636c825aa196e0dfdfc9cb
Vladimir Zhilin <VIZhilin.SBT@sberbank.ru>
2018-05-01 21:59:46 +0300

    Добавил заметку к 2.Adding mockito to the project

--- automationqa-course/11.advanced/mockito/2.Adding mockito to the project/src/test/java/ru/sbtqa/tutorials/advanced/mockito/OrderTest_Mockito_initMocks.java
+++ automationqa-course/11.advanced/mockito/2.Adding mockito to the project/src/test/java/ru/sbtqa/tutorials/advanced/mockito/OrderTest_Mockito_initMocks.java
@@ -2,22 +2,14 @@ package ru.sbtqa.tutorials.advanced.mockito;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import ru.sbtqa.tutorials.advanced.mockito.services.PromotionService;
 
 import static java.math.BigDecimal.valueOf;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
-
-/*
-Для проектов с JUnit4 нужно использовать аннотацию RunWith или механизм правил JUnit4 (аннотация
-Rule public MockitoRule mockitoRule = MockitoJUnit.rule())
- */
 
 /**
  * Tests for {@code Order} implementation.
