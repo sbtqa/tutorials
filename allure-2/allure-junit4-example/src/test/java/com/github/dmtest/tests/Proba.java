0d0dd7565b286e392c26d1df10885ed6c6f8e5ce
Dmitry Piskunov <dm.test@mail.ru>
2018-05-18 11:49:13 +0300

    add allure-junit4-example

---
+++ allure-2/allure-junit4-example/src/test/java/com/github/dmtest/tests/Proba.java
@@ -0,0 +1,9 @@
+package com.github.dmtest.tests;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Proba {
+    public static void main(String[] args) {
+        int randomNum = ThreadLocalRandom.current().nextInt(0, 0);
+    }
+}
