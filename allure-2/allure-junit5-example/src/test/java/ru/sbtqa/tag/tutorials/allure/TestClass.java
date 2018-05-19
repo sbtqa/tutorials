d70824a21c96f4b7772976cc3cbc010183b7de99
Dmitry Piskunov <dm.test@mail.ru>
2018-05-19 16:08:53 +0300

    rename packages

---
+++ allure-2/allure-junit5-example/src/test/java/ru/sbtqa/tag/tutorials/allure/TestClass.java
@@ -0,0 +1,11 @@
+package ru.sbtqa.tag.tutorials.allure;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+    @Test
+    public void simpleTest() {
+        Assertions.assertTrue(true);
+    }
+}
