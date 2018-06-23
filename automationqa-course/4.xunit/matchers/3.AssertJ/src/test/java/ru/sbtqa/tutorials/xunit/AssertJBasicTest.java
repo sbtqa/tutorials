41357f91f8a139fb8615353c1cdbfdda27c9d6f9
artsok <relix@mail.ru>
2018-06-23 09:26:24 +0300

    add example

--- automationqa-course/4.xunit/matchers/3.AssertJ/src/test/java/ru/sbtqa/tutorials/xunit/AssertJBasicTest.java
+++ automationqa-course/4.xunit/matchers/3.AssertJ/src/test/java/ru/sbtqa/tutorials/xunit/AssertJBasicTest.java
@@ -1,6 +1,7 @@
 package ru.sbtqa.tutorials.xunit;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
@@ -12,9 +13,16 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class AssertJBasicTest {
 
+    private QA autoQA1;
+    private QA autoQA2;
+
     class QA {
         private String name;
         private List<String> experience;
+
+        QA(String name) {
+            this.name = name;
+        }
 
         QA(String name, List<String> experience) {
             this.name = name;
@@ -22,19 +30,37 @@ class AssertJBasicTest {
         }
     }
 
+
     /*
-     * ============================
-     * OBJECT ASSERTIONS
-     * ============================
+     * =================================================
+     * OBJECT ASSERTIONS. Класс AbstractObjectAssert API
+     * =================================================
      */
 
     /**
-     * Сравнение двух объектов. Сравнение происходит по свойству/полю "experience" класса QA
+     * Сравнение двух объектов. Сравнение происходит по свойству/полю "experience" класса QA.
+     * Вызывается метод isEqualToComparingOnlyGivenFields у абстрактного класса AbstractObjectAssert
      */
     @Test
     void shouldWorkIsEqualToComparingOnlyGivenFields() {
-        QA autoQA1 = new QA("Artem", Collections.singletonList("SberTech"));
-        QA autoQA2 = new QA("Olga", Collections.singletonList("SberTech"));
+        autoQA1 = new QA("Artem", Collections.singletonList("SberTech"));
+        autoQA2 = new QA("Olga", Collections.singletonList("SberTech"));
         assertThat(autoQA1).isEqualToComparingOnlyGivenFields(autoQA2, "experience");
     }
+
+    /**
+     * Сравнение двух объектов, игнорирую не проиницилизированные свойства полей двух объектов
+     */
+    @Test
+    void shouldWorkIsEqualToIgnoringNullFields() {
+        autoQA1 = new QA("Dmitriy", null);
+        autoQA2 = new QA("Dmitriy", null);
+        assertThat(autoQA1).as("Сообщение при ошибке").isEqualToIgnoringNullFields(autoQA2);
+    }
+
+
+
+
+
+
 }
