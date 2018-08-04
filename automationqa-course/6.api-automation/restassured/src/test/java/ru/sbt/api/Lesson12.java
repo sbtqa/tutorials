53ec24dd14a0ce6a9aa6dbe2b56707834bb50f6d
savkk <savkk@bk.ru>
2018-08-04 13:26:00 +0300

    lesson12

--- automationqa-course/6.api-automation/restassured/src/test/java/ru/sbt/api/Lesson12.java
+++ automationqa-course/6.api-automation/restassured/src/test/java/ru/sbt/api/Lesson12.java
@@ -1,97 +1,26 @@
 package ru.sbt.api;
 
-import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 import ru.sbt.api.endpoints.EndPoints;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import java.util.Map;
+
+import static io.restassured.RestAssured.get;
 
 /**
- * GPath
+ * Для доступа к данным тела ответа в REST-assured применяется языка запросов JsonPath.
+ * JsonPath является альтернативой XPath.
+ * <p>
+ * JsonPath для навигации и доступа к данным использует Groovy GPath синаксис
+ * Данный синаксис позволяет применять фукнции-замыкания к содержимому тела ответа. Для доступа к текущему значению
+ * функции используется специальная переменная - it
  */
 
 public class Lesson12 extends RestAssuredConfig {
-    private static Integer id;
 
     @Test
-    public void getByPathParam() {
-        given()
-//                .pathParam("id", 1)
-                .when().get(EndPoints.manufacture, 1)
-                .then().log().all()
-                .and()
-                .body("title", is("Mazda")); //проверка тела ответа
-    }
-
-    @Test
-    public void getByRequestParam() {
-        given()
-                .queryParam("id", 1)
-                .when().get(EndPoints.manufactures)
-                .then().log().all().and().body("title", is("Mazda"));
-    }
-
-    @Test(priority = 1)
-    public void getAll() {
-        given()
-                .when().get(EndPoints.manufactures)
-                .then().body("size()", is(3)); // проверяем количество элементов от корневого элемента
-    }
-
-    @Test(priority = 2)
-    public void post() {
-        String newElement = "{" +
-                "  \"country\": \"Russia\"," +
-                "  \"models\": [" +
-                "    {" +
-                "      \"title\": \"Vesta Cross\"" +
-                "    }" +
-                "  ]," +
-                "  \"title\": \"Lada\"" +
-                "}";
-
-        RestAssured.responseSpecification = null;
-
-        id = given().body(newElement)
-                .when().post(EndPoints.manufactures)
-                .then()
-                .statusCode(201).log().body()
-                .and()
-                .body("id", not(isEmptyOrNullString()))
-                .and()
-                .extract().path("id");
-    }
-
-    @Test(priority = 3)
-    public void update() {
-        String updateElement = "{" +
-                "  \"country\": \"Russia\"," +
-                "  \"id\": " + id + "," +
-                "  \"models\": [" +
-                "    {" +
-                "      \"title\": \"Vesta Cross\"," +
-                "      \"title\": \"Kalina\"," +
-                "      \"title\": \"Niva 4x4\"" +
-                "    }" +
-                "  ]," +
-                "  \"title\": \"Lada\"" +
-                "}";
-
-        RestAssured.responseSpecification = null; // сбросить проверку expected status
-
-        given().body(updateElement)
-                .when().put(EndPoints.manufactures)
-                .then()
-                .statusCode(200).log().body()
-                .and()
-                .body(equalTo("{success}"));
-    }
-
-    @Test(priority = 4)
-    public void delete() {
-        given()
-                .when().delete(EndPoints.manufacture, 1)
-                .then().body(is("{success}"));
+    public void find() {
+        Map<String, ?> map = get(EndPoints.manufactures).path("find { it.id == 7}");
+        System.out.println(map);
     }
 }
