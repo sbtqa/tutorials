9e36092d71fbec86801e7318d32d9430db019fc6
artsok <relix@mail.ru>
2019-01-07 23:42:50 +0300

    init Junit 5 examples

---
+++ automationqa-course/4.xunit/junit5/src/test/java/ru/sbtqa/tutorials/xunit/LegacyJUnit4Test.java
@@ -0,0 +1,11 @@
+package ru.sbtqa.tutorials.xunit;
+
+import org.junit.Test;
+
+public class LegacyJUnit4Test {
+
+    @Test
+    public void legacySimpleTest() {
+        System.out.println("Запуск Junit 4 теста с помощью Junit 5");
+    }
+}
