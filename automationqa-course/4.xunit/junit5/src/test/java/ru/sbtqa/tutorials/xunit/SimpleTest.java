f8891b761e908022c2f1b46ab3902145ca455100
artsok <relix@mail.ru>
2019-01-07 22:44:17 +0300

    init Junit 5 examples

---
+++ automationqa-course/4.xunit/junit5/src/test/java/ru/sbtqa/tutorials/xunit/SimpleTest.java
@@ -0,0 +1,11 @@
+package ru.sbtqa.tutorials.xunit;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void simpleTest() {
+        System.out.println("Выполнение простого теста");
+    }
+}
