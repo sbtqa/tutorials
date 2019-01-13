6752cb8e68cb988494b8bf4e463548cfdbfd4270
Dmitry Piskunov <dm.test@mail.ru>
2019-01-13 22:43:32 +0300

    Add simple assertJ assertions

---
+++ automationqa-course/4.xunit/matchers/3.AssertJ/src/test/java/ru/sbtqa/tutorials/xunit/AssertJBasicAsserts.java
@@ -0,0 +1,29 @@
+package ru.sbtqa.tutorials.xunit;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AssertJBasicAsserts {
+
+    private static List<String> sovietCars = Arrays.asList("Volga", "Moskvitch", "Lada", "Zaporojets");
+
+    @Test
+    public void basicAssertTest() {
+        assertThat(sovietCars.get(0)).isEqualTo("Volga");
+        assertThat(sovietCars.get(0)).isNotEqualTo("Mercedes");
+
+        assertThat(sovietCars.get(1))
+                .startsWith("Mosk")
+                .endsWith("vitch")
+                .isEqualToIgnoringCase("moskvitch");
+
+        assertThat(sovietCars)
+                .hasSize(4)
+                .contains("Volga", "Moskvitch", "Zaporojets")
+                .doesNotContain("Audi");
+    }
+}
