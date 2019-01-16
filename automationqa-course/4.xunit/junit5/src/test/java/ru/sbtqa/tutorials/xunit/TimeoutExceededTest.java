984dff92c34f6851044fd0a561ca18e27a004ac2
artsok <relix@mail.ru>
2019-01-16 19:09:49 +0300

    add simple assertions test

--- automationqa-course/4.xunit/junit5/src/test/java/ru/sbtqa/tutorials/xunit/TimeoutExceededTest.java
+++ automationqa-course/4.xunit/junit5/src/test/java/ru/sbtqa/tutorials/xunit/TimeoutExceededTest.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 
 import static java.time.Duration.ofMillis;
 import static java.time.Duration.ofMinutes;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 class TimeoutExceededTest {
@@ -19,6 +20,13 @@ class TimeoutExceededTest {
     void timeoutExceeded() {
         assertTimeout(ofMillis(10), this::operation,
                 () -> "Операция занимает больше 10 миллисекунд");
+    }
+
+    @Test
+    void timeoutNotExceededWithResult() {
+        String actualResult = assertTimeout(ofMinutes(1),
+                () -> { return "ЕФС"; }); //Обратите внимание, лямбда в данном контексте это не дополнительное сообщение об ошибке
+        assertEquals("ЕФС", actualResult);
     }
 
     /**
